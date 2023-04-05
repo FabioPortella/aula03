2a54f9f6f6b0dff98090e8eff2c62c4ac15de331
FabioPortella <fabioportella2009@gmail.com>
2023-04-05 15:50:44 -0300

    Criando form Curso

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Cadastro from './Cadastro';
+import Curso from './Curso';
 
 function App() {
   return (
-    <Cadastro/>
+    <>
+      <Cadastro/>
+      <Curso/>
+    </>   
   );
 }
 
---
+++ src/Curso.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+export default function Curso() {
+    const [dados, setDados] = useState({
+        nome: '',
+        sigla: '',
+        semestres: 1,
+        inicio: null
+    })
+
+    function handleSubmit(event) {
+        console.log(dados);
+        event.preventDefault();
+    };
+
+    const handleChange = (event) => {
+        setDados(
+            dados => ({...dados, [event.target.name]: event.target.value})
+        )
+    }
+
+    return (
+        <div className='container mt-2'>
+            <h2>Exemplo de validação</h2>
+            <form onSubmit={handleSubmit}>
+                <label className='row mt-2'>
+                    Nome
+                    <input type="text" name='nome' onChange={handleChange} required minLength={2} maxLength={100} className="form-control" />
+                </label>
+                <label className='row mt-2'>
+                    Sigla
+                    <input type="text" name='sigla' onChange={handleChange} required pattern='^[A-Z]{3}[0-9]{4}$' className="form-control" />
+                </label>
+                <label className='row mt-2'>
+                    Quantidade de semestres
+                    <input type="number" name='semestres' onChange={handleChange} required min={1} max={12} className="form-control" />
+                </label>
+                <label className='row mt-2'>
+                    Data de início
+                    <input type="date" name='inicio' onChange={handleChange} required className="form-control" />
+                </label>
+                <button type='submit' className='btn btn-primary mt-2'>Salvar</button>
+            </form>
+        </div>
+    );
+}
