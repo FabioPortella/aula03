b1b854e1f5c1619d65e4652f1c71c0d1ccebb8d9
FabioPortella <fabioportella2009@gmail.com>
2023-04-05 18:12:28 -0300

    formatando formulário

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Cadastro from './Cadastro';
+//import Cadastro from './Cadastro';
 import Curso from './Curso';
 
 function App() {
   return (
     <>
-      <Cadastro/>
       <Curso/>
     </>   
   );
